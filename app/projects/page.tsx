7d089c462d405b15a0f2d634c7ae79d46f527b26
reverie-kyrene <107148650+reverie-syr@users.noreply.github.com>
2023-11-20 20:11:48 +0100

    feat: projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Project from "@/components/Projects/ProjectCard";
+import { Container } from "@/layouts/Container";
+import { projectsData } from "@/shared/lib/data";
+
+export default function Projects() {
+  return (
+    <Container>
+      <div>
+        <h1>
+          <span className=" block text-base font-semibold tracking-wide text-center uppercase text-indigo-500 dark:text-teal-400">
+            Projects
+          </span>
+          <span className="block max-w-2xl mx-auto mt-2 text-4xl font-bold leading-10 text-center sm:text-5xl">
+            A selection of my works.
+          </span>
+        </h1>
+      </div>
+      <br />
+      <br />
+
+      <div className="flex justify-center items-center">
+        <section className="scroll-mt-28 mb-28" id="projects">
+          <div>
+            {projectsData.map((project, index) => (
+              <React.Fragment key={index}>
+                <Project {...project} />
+              </React.Fragment>
+            ))}
+          </div>
+        </section>
+      </div>
+    </Container>
+  );
+}
--- shared/lib/data.ts
+++ shared/lib/data.ts
@@ -36,7 +36,7 @@ export const projectsData = [
   {
     title: "E-Learning",
     description:
-      " Design and development of a learning management system (LMS) with functions such as user authentication, course management, content delivery, communication with users and statistics",
+      "This application is a learning management system (LMS) with functions such as user authentication, course management, content delivery, communication with users and statistics",
     tags: [
       "Next.js",
       "TypeScript",
@@ -60,7 +60,7 @@ export const projectsData = [
   {
     title: "TechNotes",
     description:
-      "Design and development of a web application that allows users to register and authenticate themselves in order to manage their notes. The application uses access control and permissions based on the user's role, as well as JWT authorization.",
+      "TechNotes is a full-stack MERN application that uses access control and permissions based on the user's role, as well as JWT authorization.",
     tags: ["React", "Node JS", "Express JS", "MongoDB"],
     imageUrl: rmtdevImg,
     githubUrl: "https://github.com/sirine-zanina/techNotes",
@@ -96,16 +96,14 @@ export const educationData = [
     title: "Bachelor in Computer Science",
     location:
       "Higher Institute of Informatics and Communication Technologies, Sousse",
-    description:
-      "I am currently pursuing a Bachelor's degree in Computer Science at the Higher Institute of Informatics and Communication Technologies in Sousse. I am in my third year of study.",
+
     icon: React.createElement(LuGraduationCap),
     date: "2021 - 2024",
   },
   {
     title: "Baccalaureate in Mathematics",
     location: "Ibnu Sina High School, Msaken Sousse",
-    description:
-      "I obtained my baccalaureate in Mathematics with a good mention.",
+
     icon: React.createElement(LuGraduationCap),
     date: "2021",
   },
